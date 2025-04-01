023cf384571469925fe3463f9aa69cc39e39c864
dannightmare <dan.kusai.star.123234@gmail.com>
2025-04-01 16:29:31 +0300

    finished communicating_vessels.py

---
+++ src/5.4.communicating_vessels.py
@@ -0,0 +1,13 @@
+def generator_interleave(*iter):
+    if len(iter) <= 0:
+        return
+
+    max_len = max(map(len, iter))
+    for i in range(max_len):
+        for j in iter:
+            if i < len(j):
+                yield j[i]
+
+
+def interleave(*iter):
+    return list(generator_interleave(*iter))
